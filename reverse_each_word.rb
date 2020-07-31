1f10bde22704067249b3ed12e67bbc6d88f7d098
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-31 06:33:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(words)
   final = words.split(' ')
   new_arry = []
-  final.collect do |word|
+  final.each do |word|
     new_arry << word.reverse
   end
   return new_arry.join(' ')
