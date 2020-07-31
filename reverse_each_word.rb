7f3c81168823a49f17066828405440dcfe086841
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-31 06:30:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ def reverse_each_word(words)
   final = words.split(' ')
   new_arry = []
   final.each do |word|
-    new_arry << word
+    new_arry << word.reverse
+    puts word.reverse
   end
-  puts new_arry.join(' ')
 end
 
 words = "Hello there, and how are you?"
